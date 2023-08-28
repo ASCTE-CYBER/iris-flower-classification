16309daa833b4927435346b05231fdc3a00a25a4
AshtonAkridge <142553140+AshtonAkridge@users.noreply.github.com>
2023-08-28 08:59:01 -0500

    Create Diabetes.py

---
+++ Diabetes.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_diabetes # Data Set
+from sklearn.model_selection import train_test_split # Data preprocessing for ML
+from pprint import pprint # Nice format for printing
+
+data = load_diabetes()
+
+data.data
